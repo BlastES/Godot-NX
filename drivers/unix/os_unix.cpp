ea343b213c07cc1b2471884cdf0daee134087526
Blast <72162714+BlastES@users.noreply.github.com>
2026-03-14 12:18:23 +0100

    Done
    Godot 4.1.5 compiles linux editor and switch export template

--- drivers/unix/os_unix.cpp
+++ drivers/unix/os_unix.cpp
@@ -67,7 +67,9 @@
 #include <uvm/uvm_extern.h>
 #endif
 
+#if !defined(__SWITCH__)
 #include <dlfcn.h>
+#endif
 #include <errno.h>
 #include <poll.h>
 #include <signal.h>
@@ -130,12 +132,16 @@ static void handle_interrupt(int sig) {
 }
 
 void OS_Unix::initialize_debugging() {
+#if defined(__SWITCH__)
+	return;
+#else
 	if (EngineDebugger::is_active()) {
 		struct sigaction action;
 		memset(&action, 0, sizeof(action));
 		action.sa_handler = handle_interrupt;
 		sigaction(SIGINT, &action, nullptr);
 	}
+#endif
 }
 
 int OS_Unix::unix_initialize_audio(int p_audio_driver) {
@@ -152,14 +158,18 @@ void OS_Unix::initialize_core() {
 	DirAccess::make_default<DirAccessUnix>(DirAccess::ACCESS_USERDATA);
 	DirAccess::make_default<DirAccessUnix>(DirAccess::ACCESS_FILESYSTEM);
 
+#ifndef __SWITCH__
 	NetSocketPosix::make_default();
 	IPUnix::make_default();
+#endif
 
 	_setup_clock();
 }
 
 void OS_Unix::finalize_core() {
+#ifndef UNIX_SOCKET_UNAVAILABLE
 	NetSocketPosix::cleanup();
+#endif
 }
 
 Vector<String> OS_Unix::get_video_adapter_driver_info() const {
@@ -467,18 +477,20 @@ Dictionary OS_Unix::get_memory_info() const {
 	}
 #endif
 
+#ifndef __SWITCH__
 	rlimit stackinfo = {};
 	getrlimit(RLIMIT_STACK, &stackinfo);
 
 	if (stackinfo.rlim_cur != 0) {
 		meminfo["stack"] = (int64_t)stackinfo.rlim_cur;
 	}
+#endif
 
 	return meminfo;
 }
 
 Error OS_Unix::execute(const String &p_path, const List<String> &p_arguments, String *r_pipe, int *r_exitcode, bool read_stderr, Mutex *p_pipe_mutex, bool p_open_console) {
-#ifdef __EMSCRIPTEN__
+#if defined(__EMSCRIPTEN__) || defined(__SWITCH__)
 	// Don't compile this code at all to avoid undefined references.
 	// Actual virtual call goes to OS_Web.
 	ERR_FAIL_V(ERR_BUG);
@@ -552,7 +564,7 @@ Error OS_Unix::execute(const String &p_path, const List<String> &p_arguments, St
 }
 
 Error OS_Unix::create_process(const String &p_path, const List<String> &p_arguments, ProcessID *r_child_id, bool p_open_console) {
-#ifdef __EMSCRIPTEN__
+#if defined(__EMSCRIPTEN__) || defined(__SWITCH__)
 	// Don't compile this code at all to avoid undefined references.
 	// Actual virtual call goes to OS_Web.
 	ERR_FAIL_V(ERR_BUG);
@@ -592,6 +604,9 @@ Error OS_Unix::create_process(const String &p_path, const List<String> &p_argume
 }
 
 Error OS_Unix::kill(const ProcessID &p_pid) {
+#if defined(__SWITCH__)
+	return ERR_UNAVAILABLE;
+#endif
 	int ret = ::kill(p_pid, SIGKILL);
 	if (!ret) {
 		//avoid zombie process
@@ -606,11 +621,13 @@ int OS_Unix::get_process_id() const {
 }
 
 bool OS_Unix::is_process_running(const ProcessID &p_pid) const {
+#if defined __SWITCH__
+	return false;
+#endif
 	int status = 0;
 	if (waitpid(p_pid, &status, WNOHANG) != 0) {
 		return false;
 	}
-
 	return true;
 }
 
@@ -627,6 +644,7 @@ String OS_Unix::get_locale() const {
 	return locale;
 }
 
+#ifndef __SWITCH__
 Error OS_Unix::open_dynamic_library(const String p_path, void *&p_library_handle, bool p_also_set_library_path, String *r_resolved_path) {
 	String path = p_path;
 
@@ -677,6 +695,7 @@ Error OS_Unix::get_dynamic_library_symbol_handle(void *p_library_handle, const S
 	}
 	return OK;
 }
+#endif
 
 Error OS_Unix::set_cwd(const String &p_cwd) {
 	if (chdir(p_cwd.utf8().get_data()) != 0) {
